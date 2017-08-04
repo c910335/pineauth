16630967d2c823b95218e7345a23d182c9432f05
Tatsujin Chin <c910335@gmail.com>
2017-08-04 18:32:14 +0800

    use property! error and set_properties instead of if_client and if_grant
    rename authenticate_basic! to set_basic_authentication

--- src/controllers/oauth/token_controller.cr
+++ src/controllers/oauth/token_controller.cr
@@ -1,51 +1,28 @@
 module OAuth
   class TokenController < ApplicationController
     before_action do
-      all { response.content_type = "application/json" }
+      all do
+        response.content_type = "application/json"
+        set_properties
+      end
     end
 
     property! grant : Grant
     property! client : Client
     property! grant_type : String
+    property! error : String
     getter basic_auth_user : String?
     getter basic_auth_pass : String?
 
-    macro if_client
-      if authenticate_basic! && (@client = Client.authenticate(basic_auth_user, basic_auth_pass))
-        if (@grant_type = params["grant_type"]?) && ["authorization_code", "refresh_token"].includes? grant_type
-          {{yield}}
-        else
-          error :unsupported_grant_type
-        end
-      else
-        error :invalid_client
-      end
-    end
-
-    macro if_grant
-      if (@grant = Grant.find_by :code, params["code"]?) && grant.authorize client
-        if (redirect_uri = params["redirect_uri"]?) && redirect_uri != client.redirect_uri
-          error :invalid_request
-        else
-          {{yield}}
-        end
-      else
-        error :invalid_grant
-      end
-    end
-
     def create
-      if_client do
-        if_grant do
-          case grant_type
-          when "authorization_code"
-            respond_token
-          when "refresh_token"
-            refresh_and_respond_token
-          else
-            error :server_error
-          end
-        end
+      return error if error?
+      case grant_type
+      when "authorization_code"
+        respond_token
+      when "refresh_token"
+        refresh_and_respond_token
+      else
+        error :server_error
       end
     end
 
@@ -66,20 +43,27 @@ module OAuth
       "" # Todo
     end
 
-    private def error(type : Symbol)
-      {
-        error:             type.to_s,
-        error_description: "Something went wrong.",
-      }.to_json
+    private def set_properties
+      return error :invalid_client unless set_basic_authentication && (@client = Client.authenticate(basic_auth_user, basic_auth_pass))
+      return error :unsupported_grant_type unless (@grant_type = params["grant_type"]?) && ["authorization_code", "refresh_token"].includes? grant_type
+      return error :invalid_grant unless (@grant = Grant.find_by :code, params["code"]?) && grant.authorize client
+      return error :invalid_request if (redirect_uri = params["redirect_uri"]?) && redirect_uri != client.redirect_uri
     end
 
-    private def authenticate_basic!
+    private def set_basic_authentication
       if (authorization = request.headers["Authorization"]?) &&
          (basic_token = authorization[/Basic (.*)$/, 1]?) &&
          (basic_string = Base64.decode_string(basic_token))
         @basic_auth_user, @basic_auth_pass = basic_string.split(":")
         @basic_auth_user && @basic_auth_pass && true
       end
+    end
+
+    private def error(type : Symbol)
+      @error = {
+        error:             type.to_s,
+        error_description: "Something went wrong.",
+      }.to_json
     end
   end
 end
