4472fbd7d906415a4b32ec8766713ed739f397c2
Tatsujin Chin <c910335@gmail.com>
2017-10-18 21:20:05 +0800

    fix authorized applications scopes
    add has_many! to granite-orm
    add includes to granite-orm

--- config/initializers/granite_orm.cr
+++ config/initializers/granite_orm.cr
@@ -1,10 +1,16 @@
 class Granite::ORM::Base
+  macro inherited
+    @@includes_blocks : Hash(Symbol, Proc(Array({{@type}}), Nil)) = Hash(Symbol, Proc(Array({{@type}}), Nil)).new
+  end
+
   macro belongs_to!(name)
+    {% klass = name.id.capitalize %}
+
     field {{name.id}}_id : Int64
-    @{{name.id}} : {{name.id.capitalize}}?
+    @{{name.id}} : {{klass}}?
 
     def {{name.id}}?
-      @{{name.id}} ||= {{name.id.capitalize}}.find({{name.id}}_id)
+      @{{name.id}} ||= {{klass}}.find({{name.id}}_id)
     end
 
     def {{name.id}}
@@ -13,43 +19,90 @@ class Granite::ORM::Base
 
     def {{name.id}}=(parent)
       @{{name.id}}_id = parent.id
-      @{{name.id}} = nil
-      parent
+      @{{name.id}} = parent
     end
+
+    @@includes_blocks[:{{name.id}}s] = -> (children : Array({{@type}})) {
+      parents = {{klass}}.where(id: children.map(&.{{name.id}}_id))
+      parents_map = Hash(Int64 | Nil, {{klass}}).new
+      parents.each do |parent|
+        parents_map[parent.id] = parent
+      end
+      children.each do |child|
+        child.{{name.id}} = parents_map[child.{{name.id}}_id]
+      end
+      nil
+    }
+  end
+
+  macro has_many!(name, id = nil)
+    {% klass = name.id[0...-1].camelcase %}
+    {% table_name = SETTINGS[:table_name] || @type.name.gsub(/::/, "_").downcase.id + "s" %}
+    {% parent_id = id || table_name[0...-1] + "_id" %}
+
+    @{{name.id}} : Array({{klass}})?
+
+    def {{name.id}}
+      return @{{name.id}}.not_nil! unless @{{name.id}}.nil?
+      return [] of {{klass}} unless id
+      foreign_key = "{{name.id}}.{{parent_id}}"
+      query = "WHERE #{foreign_key} = ?"
+      @{{name.id}} = {{klass}}.all(query, id)
+    end
+
+    def {{name.id}}=(children)
+      @{{name.id}} = children
+    end
+
+    @@includes_blocks[:{{name.id}}] = -> (parents : Array({{@type}})) {
+      children = {{klass}}.where({{parent_id}}: parents.map(&.id))
+      parents_map = Hash(Int64 | Nil, {{@type}}).new
+      parents.each do |parent|
+        parent.{{name.id}} = [] of {{klass}}
+        parents_map[parent.id] = parent
+      end
+      children.each do |child|
+        parents_map[child.{{parent_id}}].{{name.id}} << child
+      end
+      nil
+    }
   end
 
   def self.where(**conditions)
+    conditions = conditions.to_h
+    includes = conditions.delete(:includes)
+    limit = conditions.delete(:limit)
     params = [] of DB::Any
     clause = String.build do |s|
       s << "WHERE "
       i = 0
       s << conditions.map do |k, v|
         if v
-          params << v
-          "#{k} = $#{i += 1}"
+          if v.is_a?(Array)
+            "#{k} IN (#{v.join(",")})"
+          elsif v.is_a?(DB::Any)
+            params << v
+            "#{k} = $#{i += 1}"
+          end
         else
           "#{k} IS NULL"
         end
       end.join(" AND ")
+      s << " LIMIT #{limit}" if limit
     end
-    all(clause, params)
+    results = all(clause, params)
+    if includes
+      @@includes_blocks[includes]?.try &.call results
+    end
+    results
   end
 
   def self.find_by(**conditions)
-    params = [] of DB::Any
-    clause = String.build do |s|
-      s << "WHERE "
-      i = 0
-      s << conditions.map do |k, v|
-        if v
-          params << v
-          "#{k} = $#{i += 1}"
-        else
-          "#{k} IS NULL"
-        end
-      end.join(" AND ")
-      s << " LIMIT 1"
+    results = where(**conditions, limit: 1)
+    if results.empty?
+      nil
+    else
+      results.first
     end
-    all(clause, params).first
   end
 end
--- src/models/oauth/authorized_application.cr
+++ src/models/oauth/authorized_application.cr
@@ -1,18 +1,19 @@
 require "granite_orm/adapter/pg"
 
 module OAuth
-  class AuthorizedApplication < Granite::ORM
+  class AuthorizedApplication < Granite::ORM::Base
     adapter pg
+    table_name oauth_authorized_applications
+
+    before_destroy clear_grants
 
     # id : Int64 primary key is created for you
     field revoked_at : Time
     field scopes : String
-    field user_id : Int64
-    field client_id : Int64
     timestamps
 
-    belongs_to user
-    belongs_to client
+    belongs_to! user
+    belongs_to! client
 
     def split_scopes
       if scopes = @scopes
@@ -20,6 +21,11 @@ module OAuth
       else
         [] of String
       end
+    end
+
+    def clear_grants
+      Grant.where(client_id: client_id, user_id: user_id).each &.destroy
+      AccessToken.where(client_id: client_id, user_id: user_id, revoked_at: nil).each &.revoke
     end
   end
 end
