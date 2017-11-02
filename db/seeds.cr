0d1d7976c3a84f3a9622c81bbf5b5dee8d17119f
Tatsiujin Chin <c910335@gmail.com>
2017-11-03 01:04:09 +0800

    fix seeds

--- db/seeds.cr
+++ db/seeds.cr
@@ -1,8 +1,11 @@
 require "amber"
+require "../config/initializers/*"
 require "../src/models/**"
 
-user = User.new
-user.email = "owner@example.com"
-user.password = "password"
-user.level = User::Level::Owner
-user.save
+unless User.first
+  user = User.new
+  user.email = "owner@example.com"
+  user.password = "password"
+  user.level = User::Level::Owner
+  user.save
+end
